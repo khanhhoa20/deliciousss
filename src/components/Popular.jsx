e206c00d53284d08e00395873a779560b184acb9
khanhhoa20 <khanhhoa12t2@gmail.com>
2022-06-20 22:21:12 +0700

    add some library

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,19 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function Popular() {
-
+    const [popular, setPopular] = useState([])
     useEffect(() => {
         getPopular()
     }, [])
     const getPopular = async () => {
         const api = await fetch(
-            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`
+            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
         )
         const data = await api.json();
-        console.log(data);
+
+        setPopular(data.recipes)
     }
     return (
-        <div>Popular</div>
+        <div>
+            {popular.map((recipe) => {
+                return (
+                    <div key={recipe.id}>
+                        <p>{recipe.title}</p>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
