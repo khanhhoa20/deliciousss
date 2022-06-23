0a27e50943c4e8046e91262725a30c99aab5a08e
khanhhoa20 <khanhhoa12t2@gmail.com>
2022-06-23 14:26:49 +0700

    update Category component, add Cuisine page, add routes

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+import { useState } from 'react';
+import { FaSearch } from 'react-icons/fa'
+
+function Search() {
+    return (
+        <div>Search</div>
+    )
+}
+
+export default Search
