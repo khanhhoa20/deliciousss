e17e6215a8ad06b76c89a60b7da16a93a888b7da
khanhhoa20 <khanhhoa12t2@gmail.com>
2022-08-12 01:37:15 +0700

    finish code :))

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -21,7 +21,12 @@ function Cuisine() {
         getCuisine(params.type)
     }, [params.type])
     return (
-        <Grid>
+        <Grid
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+        >
             {
                 cuisine.map((item) => {
                     return (
@@ -38,7 +43,7 @@ function Cuisine() {
     )
 }
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
 display : grid;
 grid-template-columns: repeat(auto-fit, minmax(20rem,1fr));
 grid-gap: 3rem;
